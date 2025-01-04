c8113b97926c427883738c7268f83c737807852b
Tetiana-Far <155846665+Tetiana-Far@users.noreply.github.com>
2025-01-04 14:11:13 +0300

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      This project was coded by
+      <a href="https://github.com/Tetiana-Far" rel="noreferrer" target="_blank">
+        Tetiana Farafonova
+      </a>
+      , is
+      <a
+        className="github"
+        href="https://github.com/Tetiana-Far/weather-react"
+        rel="noreferrer"
+        target="_blank"
+      >
+        open-sourced on GitHub
+      </a>
+      and
+      <a
+        href="https://playful-praline-97041e.netlify.app/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        hosted on Netlify
+      </a>
+    </footer>
+  );
+}
